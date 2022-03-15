05e7214a3176f03c712c632c83537a3f694c94f8
Bhushan <92251216+kablaze7@users.noreply.github.com>
2022-03-16 00:04:37 +0530

    Add files via upload

---
+++ Tuple_in_detail.py
@@ -0,0 +1,48 @@
+'''
+Tuple Data Type
+Tuple is represented by ( ,) then only it is called as tuple.
+A comma , should be kept/assigned to the variable/ in the print.
+Tuple can also contain several data types in it.
+Tuple is immutable, modifications cannot be made.
+'''
+
+# y="a",
+# print(y,type(y)) # ('a',) <class 'tuple'>
+
+# h=(3,6.8,'by',None)
+# print(type(h))
+# print(h[-1])
+# h[0]=5
+
+
+'''when trying to print the code which is
+already printed , will get None'''
+# c=print('hi') # hi
+# d=print(c) # None
+# print(c,type(c)) # None <class 'NoneType'>
+
+
+# x=(1)
+# print(x,type(x))
+
+# x=(1,)
+# print(x,type(x))
+
+# pr=('apple','reliance','asus','dell','samsung')
+# pr[1]="Motorolla"
+
+# n="Hello"  , "World"
+# print(n,type(n)) 
+# print(n[2])
+# v=(1,4.2,'hello',[2,3],('H','D'),None,True)
+# print(v,type(v)) 
+
+# v=(1,4.2,'hello',[2,3],('H','D'))
+# v[0]=21
+
+# v=(1,4.2,'hello',[2,3],('H','D'))
+# print(v.count(4.2))
+# print(v.index([12,3]))
+
+
+# print(dir(tuple))
